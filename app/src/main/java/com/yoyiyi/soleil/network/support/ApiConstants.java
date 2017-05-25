f3ce302093aff3e3fc056f0e0eb7fdbdb1ab79e4
zzq <2455676683@qq.com>
2017-05-25 14:35:36 +0800

    yoyiyi

--- app/src/main/java/com/yoyiyi/soleil/network/support/ApiConstants.java
+++ app/src/main/java/com/yoyiyi/soleil/network/support/ApiConstants.java
@@ -6,29 +6,29 @@ package com.yoyiyi.soleil.network.support;
  * 描述:API常量类
  */
 
-public class ApiConstants {
+public interface ApiConstants {
 
-    public static final String BILI_BASE_URL = "http://bilibili-service.daoapp.io/";
+    String LIVE_BASE_URL = "http://api.live.bilibili.com";
 
-    public static final String RANK_BASE_URL = "http://www.bilibili.com/";
+    String APP_BASE_URL = "http://app.bilibili.com/";
 
-    public static final String APP_BASE_URL = "http://app.bilibili.com/";
+    String BILI_BASE_URL = "http://bilibili-service.daoapp.io/";
 
-    public static final String LIVE_BASE_URL = "http://api.live.bilibili.com";
+    String RANK_BASE_URL = "http://www.bilibili.com/";
 
-    public static final String API_BASE_URL = "http://api.bilibili.cn/";
+    String API_BASE_URL = "http://api.bilibili.cn/";
 
-    public static final String BANGUMI_BASE_URL = "http://bangumi.bilibili.com/";
+    String BANGUMI_BASE_URL = "http://bangumi.bilibili.com/";
 
-    public static final String SEARCH_BASE_URL = "http://s.search.bilibili.com/";
+    String SEARCH_BASE_URL = "http://s.search.bilibili.com/";
 
-    public static final String ACCOUNT_BASE_URL = "https://account.bilibili.com/";
+    String ACCOUNT_BASE_URL = "https://account.bilibili.com/";
 
-    public static final String USER_BASE_URL = "http://space.bilibili.com/";
+    String USER_BASE_URL = "http://space.bilibili.com/";
 
-    public static final String VIP_BASE_URL = "http://vip.bilibili.com/";
+    String VIP_BASE_URL = "http://vip.bilibili.com/";
 
-    public static final String IM9_BASE_URL = "http://www.im9.com/";
+    String IM9_BASE_URL = "http://www.im9.com/";
 
-    public static final String COMMON_UA_STR = "BiliCopy Android Client/1.0 (soleilyoyiyi@gmail.com)";
+    String COMMON_UA_STR = "BiliSoleil Android Client/1.0 (soleilyoyiyi@gmail.com)";
 }
