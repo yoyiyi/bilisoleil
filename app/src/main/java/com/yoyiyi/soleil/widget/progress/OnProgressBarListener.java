b3aac776eae2b9ac8c9f39fad70edafbde2d892d
zzq <2455676683@qq.com>
2017-06-22 15:03:37 +0800

    yoyiyi

---
+++ app/src/main/java/com/yoyiyi/soleil/widget/progress/OnProgressBarListener.java
@@ -0,0 +1,9 @@
+package com.yoyiyi.soleil.widget.progress;
+
+/**
+ * Created by lelexxx on 15-4-23.
+ */
+public interface OnProgressBarListener {
+
+    void onProgressChange(int current, int max);
+}
