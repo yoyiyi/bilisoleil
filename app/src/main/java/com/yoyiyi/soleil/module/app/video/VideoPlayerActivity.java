7f2a1b90fcbb189d1316bbd52101697d0c561462
zzq <2455676683@qq.com>
2017-06-28 15:02:13 +0800

    yoyiyi

---
+++ app/src/main/java/com/yoyiyi/soleil/module/app/video/VideoPlayerActivity.java
@@ -0,0 +1,17 @@
+package com.yoyiyi.soleil.module.app.video;
+
+import com.yoyiyi.soleil.R;
+import com.yoyiyi.soleil.base.BaseActivity;
+
+/**
+ * @author zzq  作者 E-mail:   soleilyoyiyi@gmail.com
+ * @date 创建时间：2017/6/28 14:36
+ * 描述:视频播放界面
+ */
+
+public class VideoPlayerActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_video_player;
+    }
+}
