40a5cc37bccf858091349e7a37df009a15437f83
zzq <2455676683@qq.com>
2017-06-28 18:24:13 +0800

    yoyiyi

--- app/src/main/java/com/yoyiyi/soleil/network/api/AppService.java
+++ app/src/main/java/com/yoyiyi/soleil/network/api/AppService.java
@@ -95,7 +95,7 @@ public interface AppService {
     /**
      * 视频详情
      */
-    @GET("/x/v2/view?access_key=18b6350cf0e8fb0cacb6cf323fc2feba&aid=8659710&appkey=1d8b6e7d45233436&build=505000&from=3&mobi_app=android&plat=0&platform=android&trackid=14859045423998458858&ts=1498644965&sign=1dd38263ead25fb9bf12c09364130efb")
+    @GET("/x/v2/view?access_key=18b6350cf0e8fb0cacb6cf323fc2feba&aid=3852397&appkey=1d8b6e7d45233436&build=505000&from=3&mobi_app=android&plat=0&platform=android&trackid=14859045423998458858&ts=1498645383&sign=94a3c4d143f44f023558ea52a33be403")
     Flowable<VideoDetail> getVideoDetail();
 
     /**
