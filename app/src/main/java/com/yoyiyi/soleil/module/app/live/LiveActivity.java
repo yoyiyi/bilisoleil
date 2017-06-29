a323e7fde65a9dfe1e9185ef327debaff2db5112
zzq <2455676683@qq.com>
2017-06-29 11:42:46 +0800

    yoyiyi

---
+++ app/src/main/java/com/yoyiyi/soleil/module/app/live/LiveActivity.java
@@ -0,0 +1,20 @@
+package com.yoyiyi.soleil.module.app.live;
+
+import com.yoyiyi.soleil.R;
+import com.yoyiyi.soleil.base.BaseActivity;
+
+/**
+ * @author zzq  作者 E-mail:   soleilyoyiyi@gmail.com
+ * @date 创建时间：2017/6/29 11:38
+ * 描述:直播
+ */
+
+public class LiveActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_live;
+    }
+
+    //http://122.246.0.55/youku/65634086413D83B2531015B5E/030008FF00593908D90C2C02692D2FD5DC1465-E812-0D34-0B0F-03D7CBDCA172.mp4?sid=049870757900512632261&ctype=12&ccode=0590&duration=120&expire=18000&psid=8cbee0c45515580e4970277e8fec3464&ups_client_netip=183.156.113.189&ups_ts=1498707579&ups_userid=&utid=c1rbEdI5mFcCAbeccb16JLgD&vid=XMjgxMjEwODc1Ng%3D%3D&vkey=A544ef0fdc659731d2612b384ae07094b
+
+}
