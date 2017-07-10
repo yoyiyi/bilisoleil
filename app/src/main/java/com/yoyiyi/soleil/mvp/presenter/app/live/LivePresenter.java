2fc2e51c38984fc3f4a323d080e8958c881202b9
zzq <2455676683@qq.com>
2017-07-10 16:52:04 +0800

    yoyiyi

---
+++ app/src/main/java/com/yoyiyi/soleil/mvp/presenter/app/live/LivePresenter.java
@@ -0,0 +1,11 @@
+package com.yoyiyi.soleil.mvp.presenter.app.live;
+
+/**
+ * @author zzq  作者 E-mail:   soleilyoyiyi@gmail.com
+ * @date 创建时间：2017/7/10 16:47
+ * 描述:
+ */
+
+public class LivePresenter {
+
+}
